73c2001eeaca4501bacc5b23a5845c01927ff044
alexmar95 <alexandrupaulmarinescu@gmail.com>
2018-03-11 23:33:11 +0200

    Basically chat

---
+++ helper.c
@@ -0,0 +1,56 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <stdio.h>
+
+#include "helper.h"
+
+int set_addr(struct sockaddr_in *addr, char *name, u_int32_t inaddr, short sin_port) {
+	struct hostent *h;
+	memset((void *)addr, 0, sizeof(*addr));
+	addr->sin_family = AF_INET;
+	if (name != NULL) {
+		h = gethostbyname(name);
+		if (h == NULL)
+			return -1;
+		addr->sin_addr.s_addr = *(u_int32_t *) h->h_addr_list[0];
+	} else
+		addr->sin_addr.s_addr = htonl(inaddr);
+	addr->sin_port = htons(sin_port);
+	return 0;
+}
+
+int stream_read(int sockfd, char *buf, int len) {
+	int nread;
+	int remaining = len;
+	
+	while (remaining > 0) {
+		if ((nread = read(sockfd, buf, remaining)) == -1)
+			return -1;
+		if (nread == 0)
+			break;
+		remaining -= nread;
+		buf += nread;
+	}
+	return len - remaining;
+}
+
+int stream_write(int sockfd, char *buf, int len) {
+	int nwrite;
+	int remaining = len;
+	
+	while (remaining > 0) {
+		if ((nwrite = write(sockfd, buf, remaining)) == -1)
+			return -1;
+		remaining -= nwrite;
+		buf += nwrite;
+	}
+	return len - remaining;
+}
+
+void print_something() {
+	printf("something\n");
+}
---
+++ helper.h
@@ -0,0 +1,17 @@
+#ifndef _HELPER_H
+#define _HELPER_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <stdio.h>
+
+int set_addr(struct sockaddr_in *addr, char *name, u_int32_t inaddr, short sin_port);
+int stream_read(int sockfd, char *buf, int len);
+int stream_write(int sockfd, char *buf, int len);
+void print_something();
+
+#endif
